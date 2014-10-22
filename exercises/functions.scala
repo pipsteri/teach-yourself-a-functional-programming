f64585b91bbc273b48e0f975ffa8d279b81080d7
Pirjo Turunen <pirjo.turunen@gmail.com>
2014-10-23 01:28:39 +0300

    Update functions.scala

--- exercises/functions.scala
+++ exercises/functions.scala
@@ -1,21 +1,21 @@
 object Functions {
 
     // adds two integers together
-    def plus(x: Int, y:Int):Int = ???
+    def plus(x: Int, y:Int):Int =  x + y
     
     // salutes the given name, e.g. Hello, User
-    def salutation(name: String):String = ???
+    def salutation(name: String):String = "Hello, " + name
 
     // counts the number of characters in a given string
-    def characters(name: String):Int = ???
+    def characters(name: String):Int = name.length
 
     // returns the base x to the power y, i.e. x^y
-    def pow(x: Int, y: Int) = ???
+    def pow(x: Int, y: Int) = scala.math.pow(x,y)
 
     /** Salutes the user according to the time of day given in isDayTime
       * During daytime, salutes with "Hello". During nighttime, salutes with "Good night"
       */
-    def dayOrNightSalutation(name:String, isDayTime: Boolean) = ???
+    def dayOrNightSalutation(name:String, isDayTime: Boolean) = if(isDayTime) "Hello, " + name else "Good night, " + name
 
     // return first two names in a given list
     def firstTwo(names: List[String]) = ???
