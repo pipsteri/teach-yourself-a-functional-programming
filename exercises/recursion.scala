60e791525c1110dad8a15650f2f9fa2727dc96b5
Pirjo Turunen <pirjo.turunen@gmail.com>
2014-10-23 21:29:19 +0300

    Update recursion.scala

--- exercises/recursion.scala
+++ exercises/recursion.scala
@@ -6,20 +6,21 @@ object Recursion {
       * 5! = 5 * 4 * 3 * 2 * 1
       * 3! = 3 * 2 * 1
       */
-    def fact(num:Int) = ???
+    def fact(num:Int) =  if (num==0) 1 else num * fact(num-1)
 
     /** computes fibonacci value for a given number
       *
       * fibonacci sequence for number x can be obtained
       * by calculating fib(x - 1) + fib(x - 2)
       */
-    def fib(num:Int) = ???
+     def fib(num:Int):Int = num match { case 0 => 0  case 1 => 1 case _ => fib(num-1) + fib(num-2) }
 
     /** returns true if 'list' contains 'needle'
       *
       * Tip: for maximum learning, implement list traversal via recursion
       */
-    def contains[A](list: List[A], needle: A):Boolean = ???
+       // almost works: gives true instead of false and vice versa
+    def contains[A](list: List[A], needle: A):Boolean = list.contains("needle") == true
 
 
     /** retuns the length of the given string without invoking the length
